0318782bd08e49b6ee91d6ba7cfe4e4d3f6e2736
Nicola Caddeo <nicola.caddeo58@gmail.com>
2022-09-03 18:17:44 +0200

    added loop to create cells

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di
+// azzurro se il numero progressivo è pari, di verde se dispari.
+
+// Inoltre emette un messaggio in console con il numero della cella cliccata.
+
+// -------------------------------------------------------------------
+
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
+const gridBtn = document.getElementById('init-btn');
+const gridContainer = document.getElementById('grid');
+
+gridBtn.addEventListener('click',
+    function () {
+        gridContainer.innerHTML = '';
+
+        // ciclo per creare la griglia
+        for (i = 1; i <= 100; i++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.innerHTML = i;
+            gridContainer.append(cell);
+        }
+    }
+);
+
+
